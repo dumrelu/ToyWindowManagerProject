39e49dab33728044d8de84f21257d7ae42e633fe
Relu <dum.relu@gmail.com>
2017-06-03 18:40:00 +0300

    Start bar

--- src/com/iclp/windowmanager/Renderer.java
+++ src/com/iclp/windowmanager/Renderer.java
@@ -2,9 +2,16 @@ package com.iclp.windowmanager;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
 import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,6 +25,10 @@ public class Renderer extends Thread
     public static final int TITLE_OFFSET = 10;
     public static final int CLOSE_BUTTON_SIZE = (int) (BORDER_HEIGHT * 0.6);
     public static final int CLOSE_BUTTON_OFFSET = 10;
+    public static final int START_BAR_HEIGHT = 30;
+    public static final int START_BUTTON_WIDTH = 100;
+    public static final int START_TIME_WIDTH = 75;
+    public static final int MAX_START_BAR_LENGTH = 125;
     
     public Renderer(Manager manager, int fps)
     {
@@ -88,6 +99,8 @@ public class Renderer extends Thread
             }
             
             g.drawImage(windowFrameBuffer.getBuffer(), windowRect.x, windowRect.y, null);
+            g.setColor(Color.BLACK);
+            g.drawRoundRect(windowRect.x, windowRect.y, windowRect.width, windowRect.height, 3, 3);
             
             g.setColor(Color.DARK_GRAY);
             g.fillRect(windowRect.x, windowRect.y, windowRect.width, BORDER_HEIGHT);
@@ -111,12 +124,66 @@ public class Renderer extends Thread
             if(manager.isFocused(window))
             {
                 g.setColor(Color.YELLOW);
+                Stroke oldStroke = g.getStroke();
                 g.setStroke(new BasicStroke(3.5f));
-                g.drawRect(windowRect.x, windowRect.y, windowRect.width, windowRect.height);
+                g.drawRoundRect(windowRect.x, windowRect.y, windowRect.width, windowRect.height, 3, 3);
+                g.setStroke(oldStroke);
             }
+
             
             windowFrameBuffer.endRender(windowG);
         }
+        
+        //Start bar
+        AffineTransform oldTransform = g.getTransform();
+        g.setColor(Color.LIGHT_GRAY);
+        g.translate(0, desktop.getBuffer().getHeight() - START_BAR_HEIGHT);
+        g.fillRect(0, 0, desktop.getBuffer().getWidth(), START_BAR_HEIGHT);
+        g.setColor(Color.BLACK);
+        g.drawRect(0, 0, desktop.getBuffer().getWidth(), START_BAR_HEIGHT);
+
+
+        //Start button
+        g.setColor(Color.GREEN);
+        g.fillRect(0, 0, START_BUTTON_WIDTH, START_BAR_HEIGHT);
+        g.setColor(Color.BLACK);
+        g.drawRect(0, 0, START_BUTTON_WIDTH, START_BAR_HEIGHT);
+        Font oldFont = g.getFont();
+        g.setFont(new Font("TimesRoman", Font.PLAIN, 25)); 
+        int fontHeight = Math.round(getTextHeight(g, "Start"));
+        g.drawString("Start", TITLE_OFFSET, (int) (fontHeight * 0.8));
+        
+        //Start bar time
+        Date currentDate = new Date();
+        g.setColor(Color.BLACK);
+        g.drawRect(desktop.getBuffer().getWidth() - START_TIME_WIDTH, 0, START_TIME_WIDTH, START_BAR_HEIGHT);
+        g.drawString(currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds(), desktop.getBuffer().getWidth() - START_TIME_WIDTH, (int) (fontHeight * 0.8));
+        
+        //Startbar applications
+        g.setFont(new Font("TimesRoman", Font.PLAIN, 15));
+        int xStart = START_BUTTON_WIDTH;
+        int rectLength = getStartBarWindowLength(desktop.getBuffer().getWidth(), this.manager.getWindows(desktop).size());
+        ArrayList<Window> windows = new ArrayList<>(this.manager.getWindows(desktop));
+        Collections.sort(windows, new WindowNameComparator());
+        for(Window window : windows)
+        {
+            g.setColor(new Color(230, 230, 230));
+            g.fillRect(xStart, 0, rectLength, START_BAR_HEIGHT);
+            
+            g.setColor(Color.BLACK);
+            g.drawRect(xStart, 0, rectLength, START_BAR_HEIGHT);
+            
+            if(manager.isFocused(window))
+            {
+                g.setColor(Color.YELLOW);
+            }
+                
+            drawCenteredString(g, window.getTitle(), new Rectangle(xStart, 0, rectLength, START_BAR_HEIGHT));
+            
+            xStart += rectLength;
+        }
+
+        g.setTransform(oldTransform);
         
         frameBuffer.endRender(g);
     }
@@ -134,5 +201,43 @@ public class Renderer extends Thread
     {
         FontRenderContext fcc = g2.getFontRenderContext();
         return g2.getFont().getLineMetrics(text, fcc).getHeight();
+    }
+    
+    private int getStartBarWindowLength(int desktopWidth, int numOfWindows)
+    {
+        if(numOfWindows == 0)
+        {
+            return 0;
+        }
+        
+        desktopWidth -= START_BUTTON_WIDTH + START_TIME_WIDTH;
+        int length = desktopWidth / numOfWindows;
+        if(length > MAX_START_BAR_LENGTH)
+        {
+            return MAX_START_BAR_LENGTH;
+        }
+        return length;
+    }
+    
+    public void drawCenteredString(Graphics2D g, String text, Rectangle rect) 
+    {
+        // Get the FontMetrics
+        FontMetrics metrics = g.getFontMetrics(g.getFont());
+        // Determine the X coordinate for the text
+        int x = rect.x + (rect.width - metrics.stringWidth(text)) / 2;
+        // Determine the Y coordinate for the text (note we add the ascent, as in java 2d 0 is top of the screen)
+        int y = rect.y + ((rect.height - metrics.getHeight()) / 2) + metrics.getAscent();
+        // Draw the String
+        g.drawString(text, x, y);
+    }
+    
+    private class WindowNameComparator implements Comparator<Window>
+    {
+        @Override
+        public int compare(Window o1, Window o2) 
+        {
+            return o1.getTitle().compareTo(o2.getTitle());
+        }
+        
     }
 }
